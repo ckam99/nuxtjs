b044e98338f96e2ef4f2d594e332060b861322e6
Ckam <ckam225@gmail.com>
2021-10-14 12:46:32 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+
+
+</script>
+<template>
+    <div class="container">
+        <h1>Wecome Nuxt 3</h1>
+    </div>
+</template>
+
+<style>
+.container {
+    width: 60%;
+    margin: 60px auto;
+    padding: 40px;
+    text-align: center;
+    background: #fff;
+}
+</style>
